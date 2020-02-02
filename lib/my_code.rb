89b2776c73b0395f6a3bb38b36c0181bd0a39580
Spencer Blum <spencerblum@gmail.com>
2020-02-02 18:58:36 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,7 +6,7 @@ def map_to_negativize(source_array)
 
   new_array = []
 
-  for i in 0...source_array.length do
+  for i in 0...source_array.length
       new_array.push(source_array[i] * -1)
   end
 
